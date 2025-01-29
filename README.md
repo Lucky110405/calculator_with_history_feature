3ae3de659def5be632e087f77bbf79afa2403a37
Lucky110405 <mlucky110405@gmail.com>
2025-01-30 02:35:58 +0530

    creating basic calculator logic in js

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 learnt adding event listener to multiple elements of a class.
-learnt why let is used declare i in a for loop (to ensure that it has a local scope , if left undeclared it would have global scope which will lead to unexpected behaviour )
-learnt we have to use innertext or textcontent in place of value property when accessing an element other than form elements 
+learnt why let is used declare i in a for loop (to ensure that it has a local scope , if left undeclared it would have global scope which will lead to unexpected behaviour).
+learnt we have to use innertext or textcontent in place of value property when accessing an element other than form elements.
+learnt (got forgotten for a bit) that for an input element you have to use value property not innertext property.
+learnt that (got forgotten) to clear the value of an input field you have to set its value as an empty string ( "" ).
+learnt a bit more about includes() method.
+learnt string split() method to convert a string into an array of strings.
--- script.js
+++ script.js
@@ -1,14 +1,64 @@
 document.addEventListener('DOMContentLoaded', () => {
+    
     const numberButtonsClass = document.getElementsByClassName('numberButtons');
-    for (let i = 0 ; i < numberButtonsClass.length ; i++) {
-        numberButtonsClass[i].addEventListener('click', () => {
-            const numberValue = numberButtonsClass[i].innerText;
-            enterNumber(numberValue);
-        })
+    const arithmeticOperationButtonsClass = document.getElementsByClassName('arithmeticOperationButtons');
+    const clearButton = document.getElementById('clear');
+    const equalButton = document.getElementById('equal');
+
+
+    if (numberButtonsClass) {
+        for (let i = 0 ; i < numberButtonsClass.length ; i++) {
+            numberButtonsClass[i].addEventListener('click', () => {
+                const numberValue = numberButtonsClass[i].innerText;
+                enterValue(numberValue);
+            })
+        }
     }
+
+    if (arithmeticOperationButtonsClass) {
+        for (let i = 0 ; i < arithmeticOperationButtonsClass.length ; i++) {
+            arithmeticOperationButtonsClass[i].addEventListener('click', () => {
+                const operationValue = arithmeticOperationButtonsClass[i].innerText;
+                enterValue(operationValue);
+            })
+        }
+    }
+    
+    clearButton.addEventListener('click', () => {
+        clearValue();
+    })
+
+    equalButton.addEventListener('click', () => {
+        calculate();
+    })
+
 });
 
-function enterNumber(numberValue) {
+function enterValue(Value) {
     const calculatorOutputText = document.getElementById('calculatorOutputText');
-    calculatorOutputText.innerText += `${numberValue}`;
+    if (calculatorOutputText) {
+        const isarithmeticOperators = "+ - * /";
+        isarithmeticOperators.includes(Value) ? calculatorOutputText.value += ` ${Value} ` : calculatorOutputText.value += `${Value}`;
+    
+    }  
+}
+
+function clearValue() {
+    const calculatorOutputText = document.getElementById('calculatorOutputText');
+    if (calculatorOutputText) {
+        calculatorOutputText.value = "0";
+    }
+}
+
+function calculate() {
+    const calculatorOutputText = document.getElementById('calculatorOutputText');
+    const splitedArray = calculatorOutputText.value.split(" ");
+    
+    console.log(splitedArray);
+    const array = splitedArray.forEach(element => {
+        Number(element);
+    });
+    console.log(array);
+
+    calculatorOutputText.value = calculatedValue;
 }
